15885525af1cdb3d81b96a3b4f3d572f789478db
DaDa-shang <shangziyue2003@126.com>
2023-09-14 22:24:03 -0700

    Initial Commit

---
+++ Cubstart Demo/Cubstart_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cubstart_DemoApp.swift
+//  Cubstart Demo
+//
+//  Created by 商子岳 on 2023/9/14.
+//
+
+import SwiftUI
+
+@main
+struct Cubstart_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
